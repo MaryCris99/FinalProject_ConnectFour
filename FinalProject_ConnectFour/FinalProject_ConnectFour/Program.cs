94d6423c8bdaaddbdad4d665b6b8d29b3f0c6171
MaryCris99 <m.alegria357@mybvc.ca>
2023-06-12 00:09:44 -0600

    Classes
    Creating classes that I might need to run my Connect Four program.

---
+++ FinalProject_ConnectFour/FinalProject_ConnectFour/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+public class ConnectFour
+{
+    
+    private char currentPlayer;
+    private bool gameOver;
+    private const int Rows = 6;
+    private const int Columns = 7;
+
+    public ConnectFour()
+    {
+        
+    }
+
+   
+
+    public void Play()
+    {
+        Console.WriteLine("**********Welcome to Connect Four!************");
+
+        //if statement
+
+
+            else
+            {
+                Console.WriteLine("Invalid move! Please try again.");
+            }
+        }
+
+        Console.WriteLine("Thanks for playing Connect Four!");
+    }
+
+    private void Display()
+    {
+        
+    }
+
+    private int GetColumn()
+    {
+        
+    }
+
+    private bool MakeMove(int column)
+    {
+        //if else statement, 
+    }
+
+    private bool CheckWin(char player)
+    {
+        // Check horizontal
+        
+
+        // Check vertical
+       
+        
+
+        // Check diagonal 
+        
+        
+
+       // return false;
+    }
+
+    private bool CheckDraw()
+    {
+        //check draw
+    }
+
+    private void SwitchPlayer()
+    {
+        //switch player
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Play();
+    }
+}
