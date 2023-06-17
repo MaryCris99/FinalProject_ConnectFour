4a9d7c650788c2c1292afc6815c7a4fc9eda5d5c
MaryCris99 <127761994+MaryCris99@users.noreply.github.com>
2023-06-17 00:13:14 -0600

    Adding Display and Get Column Function
    - it needs some modifications because it didn't meet the requirements of the project, making it not less than 4 Classes.

--- FinalProject_ConnectFour/FinalProject_ConnectFour/Program.cs
+++ FinalProject_ConnectFour/FinalProject_ConnectFour/Program.cs
@@ -54,12 +54,34 @@ public class ConnectFour
     
     private void Display()
     {
-        
+        Console.WriteLine();
+        for (int row = Rows - 1; row >= 0; row--)
+        {
+            Console.Write("|");
+            for (int col = 0; col < Columns; col++)
+            {
+                Console.Write($"{board[row, col]}|");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("---------------");
+        Console.WriteLine(" 1 2 3 4 5 6 7");
+        Console.WriteLine();
     }
 
     private int GetColumn()
     {
-        
+         int column;
+        while (true)
+        {
+            Console.Write($"Player {currentPlayer}, enter a column (1-{Columns}): ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out column) && column >= 1 && column <= Columns)
+            {
+                return column - 1; // Subtract 1 to convert to 0-based index
+            }
+            Console.WriteLine("Invalid column. Please try again.");
+        }
     }
 
     private bool MakeMove(int column)
