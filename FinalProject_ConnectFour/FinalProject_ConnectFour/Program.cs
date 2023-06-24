1b97c53b623285b3a6d21e050cc2464d734f3f39
MaryCris99 <127761994+MaryCris99@users.noreply.github.com>
2023-06-23 21:47:01 -0600

    Adding comments in each line and how each method works

--- FinalProject_ConnectFour/FinalProject_ConnectFour/Program.cs
+++ FinalProject_ConnectFour/FinalProject_ConnectFour/Program.cs
@@ -1,22 +1,25 @@
 using System;
 
 // Interface for a player
-interface IPlayer
+interface IPlayer 
 {
-    int PlayMove();
+    int PlayMove();// This method is declared as an abstract method, meaning it does not have an implementation in the base class. 
 }
 
+//Note: The subclasses derived from  "Player" will need to override and create its own implementation.
 // Base class for a player
-abstract class Player : IPlayer
+abstract class Player : IPlayer // define an abstract base class "Player" that implements "IPlayer" interface.
 {
+    // "name" and "symbol" are protected instance variables that store the player's name and symbol.
+    // The "protected" access modifier allows these variables to be accessed by derived classes.
     protected string name;
     protected char symbol;
 
-    protected internal string Name
+    protected internal string Name // is a protected internal property that provides read only access to the "name"
     {
         get { return name; }
     }
-    protected internal char Symbol
+    protected internal char Symbol // is a protected internal property that provides read only access to the "symbol"
     {
         get { return symbol; }
     }
@@ -31,15 +34,17 @@ abstract class Player : IPlayer
 }
 
 // Human player 1 class inheriting from Player
+
 class HumanPlayer1 : Player
 {
-    public HumanPlayer1(string name, char symbol) : base(name, symbol)
+    //calling the base class constructor using the base (name,symbol) to initialize the name and symbol of the player.
+    public HumanPlayer1(string name, char symbol) : base(name, symbol) 
     {
     }
 
-    public override int PlayMove()
+    public override int PlayMove()  //provide its own implementations for the PlayMove() method.
     {
-        Console.WriteLine($"[{name}], it's your turn ({symbol}). Enter a column (1-7)::");
+        Console.WriteLine($"[{name}], it's your turn ({symbol}). Enter a column (1-7):");
         int column = Convert.ToInt32(Console.ReadLine());
         return column;
     }
@@ -54,7 +59,7 @@ class HumanPlayer2 : Player
 
     public override int PlayMove()
     {
-        Console.WriteLine($"[{name}], it's your turn ({symbol}). Enter a column (1-7)::");
+        Console.WriteLine($"[{name}], it's your turn ({symbol}). Enter a column (1-7):");
         int column = Convert.ToInt32(Console.ReadLine());
         return column;
     }
@@ -63,10 +68,10 @@ class HumanPlayer2 : Player
 // Connect Four game class
 class ConnectFour
 {
-    private char[,] board;
-    private Player player1;
+    private char[,] board; // is a character array representing the board game.
+    private Player player1; // instances of the "Player" class and representing the player of the game
     private Player player2;
-    private bool gameOver;
+    private bool gameOver; //boolean variable indicating wheter the game has ended or not.
 
     public ConnectFour(Player player1, Player player2)
     {
@@ -76,7 +81,8 @@ class ConnectFour
         gameOver = false;
     }
 
-    public void StartGame()
+    //
+    public void StartGame() // This method represents and indicates the control flows of the Connect Four Game.
     {
         Console.WriteLine("*****Welcome to Connect Four Game!*****");
 
@@ -88,46 +94,46 @@ class ConnectFour
             Console.WriteLine("\nCurrent Board:");
             DisplayBoard();
 
-            int column = currentPlayer.PlayMove();
+            int column = currentPlayer.PlayMove(); // the current player is prompted to make a move by calling the "PlayMove()" method of the current player
 
-            if (MakeMove(column))
+            if (MakeMove(column)) // it calls the column number that has been chosen by the player, if it is valid, the method proceeds
             {
-                int row = DropPiece(column, currentPlayer.Symbol);
-                if (IsWinningMove(row, column, currentPlayer.Symbol))
+                int row = DropPiece(column, currentPlayer.Symbol); // this method is called to drop the player's symbol in the specified column.
+                if (IsWinningMove(row, column, currentPlayer.Symbol)) // this method is called to check if the player has won the game
                 {
-                    Console.WriteLine($"\n[{currentPlayer.Name}] Wins! Congratulations!");
-                    gameOver = true;
+                    Console.WriteLine($"\n[{currentPlayer.Name}] Wins! Congratulations!");// message is displayed if the player won the game
+                    gameOver = true; // true if the player has won the game. 
                 }
-                else if (IsBoardFull())
+                else if (IsBoardFull()) // this method id used to check if the game board is full and there is no winner
                 {
                     Console.WriteLine("\nThe game is a draw.");
-                    gameOver = true;
+                    gameOver = true; 
                 }
                 else
-                {
+                {// If neither of the player wins nor the board is true, the turn switches to the next player and the "moveCount" is incremented.
                     currentPlayer = (currentPlayer == player1) ? player2 : player1;
-                    moveCount++;
+                    moveCount++; 
                 }
             }
             else
             {
-                Console.WriteLine("Invalid move. Please try again.");
+                Console.WriteLine("Invalid move. Please try again."); // this message will be displayed 
             }
         }
 
         Console.WriteLine("\nGame Over.");
     }
 
-    private bool MakeMove(int column)
+    private bool MakeMove(int column) //This method checks if the specified column is within the valid range from 1-7 and if the topmost cell in that column is empty ('\0')
     {
         return column >= 1 && column <= 7 && board[0, column - 1] == '\0';
     }
 
-    private int DropPiece(int column, char symbol)
+    private int DropPiece(int column, char symbol) // this method drops a symbol in the specified column by finding the lowest available row in that column.
     {
         for (int row = 5; row >= 0; row--)
         {
-            if (board[row, column - 1] == '\0')
+            if (board[row, column - 1] == '\0') 
             {
                 board[row, column - 1] = symbol;
                 return row;
@@ -136,7 +142,7 @@ class ConnectFour
         return -1;
     }
 
-    private bool IsWinningMove(int row, int column, char symbol)
+    private bool IsWinningMove(int row, int column, char symbol)// this method checks if the most recent move made by a player results in a winning
     {
         int count = 0;
 
@@ -198,7 +204,7 @@ class ConnectFour
         return false;
     }
 
-    private bool IsBoardFull()
+    private bool IsBoardFull() // this method checks if the game board is completely filled with player symbols. 
     {
         for (int r = 0; r < 6; r++)
         {
@@ -211,8 +217,9 @@ class ConnectFour
         return true;
     }
 
-    private void DisplayBoard()
+    private void DisplayBoard() // display the current state of the game board. The board is displayed as a grid of rows and columns separated by vertical lines ("|")
     {
+        Console.Write("");
         for (int r = 0; r < 6; r++)
         {
             Console.Write("|");
@@ -225,7 +232,7 @@ class ConnectFour
         Console.WriteLine("--------------");
         for (int col = 0; col < 7; col++)
         {
-            Console.Write((col + 1) + " ");
+            Console.Write((col+ 1) + " ");
         }
         Console.WriteLine();
 
@@ -235,12 +242,13 @@ class ConnectFour
 // Main program
 class Program
 {
+    // Entry point for the program.
     static void Main(string[] args)
     {
         Player player1 = new HumanPlayer1("Player 1", 'X');
         Player player2 = new HumanPlayer2("Player 2", 'O');
 
         ConnectFour game = new ConnectFour(player1, player2);
-        game.StartGame();
+        game.StartGame(); // game is started by calling the "StartGame()" method
     }
 }
